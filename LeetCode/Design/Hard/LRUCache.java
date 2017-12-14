09c4a6b85ff9468d9c113b35b82871eb2e8f2e0e
krishnakannan <k.krish@yahoo.com>
2017-12-14 23:58:01 +0800

    Correcting LRU Indents

--- LeetCode/Design/Hard/LRUCache.java
+++ LeetCode/Design/Hard/LRUCache.java
@@ -3,25 +3,25 @@ class LRUCache {
 
 	//Create a Doubly LinkedList and a Queue over it.
 	class ListNode {
-        int key;
+		int key;
 		int val;
 		ListNode prev;
 		ListNode next;
 		public ListNode(int key, int val) { 
-            this.key = key;
+			this.key = key;
 			this.val = val;
 		}
-        
-        public void setVal(int val) {
-            this.val = val;
-        }
+
+		public void setVal(int val) {
+			this.val = val;
+		}
 	}
 
 	//Necessary DS
 	ListNode head;
 	ListNode tail;
 	Map<Integer, ListNode> cacheMap;
-    int capacity;
+	int capacity;
 
 	//LinkedList Operations - Insert at head, Remove at last, Remove at middle. Print List
 	//Q Operation - Insert at Head
@@ -37,32 +37,32 @@ class LRUCache {
 			head.prev = newNode;
 			head = newNode;
 		}
-        cacheMap.put(key, newNode);
+		cacheMap.put(key, newNode);
 		return newNode;
 	}
 
-    //Remove CurrentNode from Queue
+	//Remove CurrentNode from Queue
 	public void remove(ListNode node) {
 		//Remove at head
 		if (node.prev == null) {
 			head = head.next;
-            if (node.next != null) {
-                node.next.prev = null;    
-            }			
+			if (node.next != null) {
+				node.next.prev = null;    
+			}			
 			node.next = null;
 		} else if (node.next == null) {
 			//Remove at tail
 			tail = tail.prev;
-            if (node.prev != null) {
-                node.prev.next = null;    
-            }			
+			if (node.prev != null) {
+				node.prev.next = null;    
+			}			
 			node.prev = null;
 		} else {            
-            // Remove at middle
+			// Remove at middle
 			node.prev.next = node.next;
 			node.next.prev = node.prev;	
-            node.next = null;
-            node.prev = null;
+			node.next = null;
+			node.prev = null;
 		}        
 	}
 
@@ -75,56 +75,55 @@ class LRUCache {
 				removed.prev.next = null;
 				removed.prev = null;	
 			}			
-            cacheMap.remove(removed.key);
+			cacheMap.remove(removed.key);
 		}        
-        
 		return removed;	
 	}
 
 
-    public LRUCache(int capacity) {
-        head = null;
-        tail = null;
-        cacheMap = new HashMap<>();
-        this.capacity = capacity;
-    }
-    
-    public int get(int key) {
-        
-        if (cacheMap.containsKey(key)) {
-            //Get Node remove it and insert it to maintain the LRU order
-        	ListNode node = cacheMap.get(key);
-        	remove(node);
-        	add(node.key, node.val);            
-        	return node.val;
-        } else {
-        	return -1;
-        }
-    }
-    
-    public void put(int key, int value) {
-    	//There is already value in Queue - change it remove it and insert it. to maintain LRU Order         
-        if (cacheMap.containsKey(key)) {        	  
-            ListNode node = cacheMap.get(key);
-            node.val = value;
-        	remove(node);            
-        	add(node.key, node.val);            
-        } else {
-            if (cacheMap.size() < capacity) {
-                //Add or Modify value in map/queue
-                add(key, value);                                  
-            } else {                
-                //Add or Modify value in map/queue and poll last element
-                add(key, value);                    
-                poll();                                          	                                    
-            }        	
-        }        
-    }
+	public LRUCache(int capacity) {
+		head = null;
+		tail = null;
+		cacheMap = new HashMap<>();
+		this.capacity = capacity;
+	}
+
+	public int get(int key) {
+
+		if (cacheMap.containsKey(key)) {
+			//Get Node remove it and insert it to maintain the LRU order
+			ListNode node = cacheMap.get(key);
+			remove(node);
+			add(node.key, node.val);            
+			return node.val;
+		} else {
+			return -1;
+		}
+	}
+
+	public void put(int key, int value) {
+		//There is already value in Queue - change it remove it and insert it. to maintain LRU Order         
+		if (cacheMap.containsKey(key)) {        	  
+			ListNode node = cacheMap.get(key);
+			node.val = value;
+			remove(node);            
+			add(node.key, node.val);            
+		} else {
+			if (cacheMap.size() < capacity) {
+				//Add or Modify value in map/queue
+				add(key, value);                                  
+			} else {                
+				//Add or Modify value in map/queue and poll last element
+				add(key, value);                    
+				poll();                                          	                                    
+			}        	
+		}        
+	}
 }
 
 /**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
+* Your LRUCache object will be instantiated and called as such:
+* LRUCache obj = new LRUCache(capacity);
+* int param_1 = obj.get(key);
+* obj.put(key,value);
+*/
